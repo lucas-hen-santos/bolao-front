19809d7c5b44952123fafcbc3ed2c7efa990d208
LUCAS HENRIQUE DOS SANTOS <86979899+lucas-hen-santos@users.noreply.github.com>
2026-03-20 08:33:58 -0300

    Update API and image base URLs in environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://bolao-api-f5d5.onrender.com/api/v1', 
-  imageBaseUrl: 'https://bolao-api-f5d5.onrender.com'
+  apiUrl: 'https://bolao-api-sd8m.onrender.com/api/v1', 
+  imageBaseUrl: 'https://bolao-api-sd8m.onrender.com'
 };
